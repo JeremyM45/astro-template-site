25e48143a9b9f19bcacbbe238591048161296dcf
JeremyM45 <jeremyvs1945@gmail.com>
2022-10-12 21:52:23 -0700

    refined dynamic dropdown

--- src/components/Navbar/DynamicDropDown.jsx
+++ src/components/Navbar/DynamicDropDown.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 import DynamicDropMenuItem from './DynamicDropMenuItem';
 
-export default function DynamicDropDown({ title, links}){
-
+export default function DynamicDropDown({ cardData }){
+  
   const [isVisible, setIsVisible] = useState(false);
-  const [visibleCompnent, setvisibleCompnent] = useState(99);
+  const [visibleCompnent, setvisibleCompnent] = useState(0);
   const [componenData, setComponentData] = useState({});
-
+  
   function handleDynamicMenu(data){
-    if(!isVisible){setIsVisible(true)}
-    if(visibleCompnent != data.key){setvisibleCompnent(data.key)}
-    if(componenData != data.link){setComponentData(data)}
+    console.log(data)
+    if(!isVisible){console.log('test visable'), setIsVisible(true)}
+    if(visibleCompnent != data.key){console.log('component'), setvisibleCompnent(data.key)}
+    if(componenData != data){console.log('test data'), setComponentData(data)}
   }
 
   return (
@@ -18,21 +19,21 @@ export default function DynamicDropDown({ title, links}){
       <div className="relative group" >
         
         <button className="flex flex-row border-b border-transparent items-center w-full px-4 py-4 mt-2 text-base text-left bg-transparent rounded-lg md:w-auto md:inline md:mt-0 md:ml-4 focus:outline-none hover:border-red-300 hover:text-red-300">
-            <span>{title}</span>
+            <span>Dynamic Dropdown</span>
         </button>
         <div className="absolute z-10 hidden bg-grey-200 group-hover:block">
         <DynamicDropMenuItem isVisible={isVisible} data={componenData} visibleComponent={visibleCompnent}/>
             <div className="px-2 w-60 pt-2 pb-4 bg-white shadow-lg rounded-lg">
               <ul className="grid grid-cols-1 gap-4 p-2">
                 {
-                  links.map(function(link, i) {
-                    const data = {link: link, key: i}
+                  cardData.map(function(data, i) {
+                    const componentData = {data: data, key: i}
                     return (
-                      <div key={i} onMouseOver={() => handleDynamicMenu(data)}>
+                      <div key={i} onMouseOver={() => handleDynamicMenu(componentData)}>
                         <li>
                           <div >
                             <a  href="/"  className="flex flex-row mr-6 p-2 border-b border-transparent hover:border-red-300 hover:text-red-300">
-                              <span >{link}</span>
+                              <span >{data.title}</span>
                             </a>
                           </div>
                         </li>
--- src/components/Navbar/DynamicDropMenuItem.jsx
+++ src/components/Navbar/DynamicDropMenuItem.jsx
@@ -1,14 +1,38 @@
-export default function DynamicDropMenuItem({ isVisible, data, visibleComponent }){
+import DynamicMenusCard from "./DynamicMenuCard"
 
+export default function DynamicDropMenuItem({ isVisible, data, visibleComponent }){
   return(
     <>
       {isVisible && visibleComponent === data.key ?
-        <div className='absolute p-40 px-80 border border-lime-300 left-60 bg-white'>
-          <div className="flex">
-            <h1 className="absolute left-0 top-0 p-6 text-lg">{data.link}</h1>
+        <div className='absolute p-60 px-80 left-60 bg-white'>
+          <div className="absolute left-0 top-0">
+              <h1 className="border-b border-slate-300  mx-6 mt-4 pb-2 text-lg pt-2">{data.title} Product Overview</h1>
+              <div className="flex flex-row">
+                <ul className="flex flex-col w-full space-y-6 mx-4 pt-4">
+                  {data.data.listItems.map(function(item, i){
+                    ;
+                    return(
+                    <li key={i} className="mt-4 w-full">
+                    <a href="#">{item}</a>
+                  </li>
+                  )
+                  })}
+                  
+                </ul>
+                <div className="grid grid-cols-2">
+                  <DynamicMenusCard data={data.data.cards[0]}/>
+                  <DynamicMenusCard data={data.data.cards[1]}/>
+                  <DynamicMenusCard data={data.data.cards[2]}/>
+                  <DynamicMenusCard data={data.data.cards[3]}/>
+                </div>
+              </div>
+              
+            
+              
           </div>
-          
+                
         </div>
+        
         :
         <></>
       }
