97a2210664554e1bb386926208def86b2b7ced6f
JeremyM45 <jeremyvs1945@gmail.com>
2022-10-14 19:50:23 -0700

    adjust body card styling

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -20,9 +20,9 @@ export default function({ videoId }: videoProps){
               height="400" 
               src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} 
               title="YouTube video player" 
-              frameborder="0" 
+              frameBorder="0" 
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
         : 
